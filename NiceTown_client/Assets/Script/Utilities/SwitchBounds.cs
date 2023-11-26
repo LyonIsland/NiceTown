da2c2a3ce202e2605c2cd513b861b1c41081d174
island <892340809@qq.com>
2023-11-26 20:19:07 +0800

    Player Action

---
+++ NiceTown_client/Assets/Script/Utilities/SwitchBounds.cs
@@ -0,0 +1,19 @@
+using Cinemachine;
+using UnityEngine;
+
+public class SwitchBounds : MonoBehaviour
+{
+    //TODO: ÇÐ»»³¡¾°µ÷ÓÃ
+    private void Start()
+    {
+        SwitchConfinerShape();
+    }
+    private void SwitchConfinerShape()
+    {
+        PolygonCollider2D confinerShape = GameObject.FindGameObjectWithTag("BoundsConfiner").GetComponent<PolygonCollider2D>();
+        CinemachineConfiner confiner = GetComponent <CinemachineConfiner> ();
+        confiner.m_BoundingShape2D = confinerShape;
+
+        confiner.InvalidatePathCache();
+    }
+}
