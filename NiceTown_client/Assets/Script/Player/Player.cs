8e216c8be3412087fed45a1b0c7a4f0fcbfc8a61
Island <50571463+Lijiale233@users.noreply.github.com>
2023-11-27 22:14:09 +0800

    player complete

--- NiceTown_client/Assets/Script/Player/Player.cs
+++ NiceTown_client/Assets/Script/Player/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class Player : MonoBehaviour
 {
@@ -10,16 +11,29 @@ public class Player : MonoBehaviour
     private float inputX;
     private float inputY;
     private Vector2 movmentInput;
+    private bool isMoving;
+    private Animator[] animators;
+    
     private void Awake()
-    {
+    { 
         rb = GetComponent<Rigidbody2D>();
+        animators = GetComponentsInChildren<Animator>();
+    
     }
     private void PlayerInput()
     {
-        inputX = Input.GetAxisRaw("Horizontal");
-        inputY = Input.GetAxisRaw("Vertical");
-
-        movmentInput = new Vector2(inputX, inputY).normalized;
+        if(Input.GetKey(KeyCode.LeftShift)){
+            inputX = Input.GetAxisRaw("Horizontal");
+            inputY = Input.GetAxisRaw("Vertical");
+            movmentInput = new Vector2(inputX, inputY).normalized;
+        }else{
+            inputX = Input.GetAxisRaw("Horizontal")*0.5f;
+            inputY = Input.GetAxisRaw("Vertical")*0.5f;
+            movmentInput = new Vector2(inputX, inputY).normalized*0.5f;
+        }
+        
+        
+        isMoving = movmentInput != Vector2.zero;
     }
     private void Movement()
     {
@@ -28,9 +42,27 @@ public class Player : MonoBehaviour
     }
 
     private void FixedUpdate()
-    {
+    {                                                
         PlayerInput();
         Movement();
+    }
+
+    private void Update(){
+        SwitchAnimation();
+    }
+
+
+    private void SwitchAnimation()
+    {
+        foreach(var anim in animators)
+        {
+            anim.SetBool("isMoving",isMoving);
+            if (isMoving)
+            {
+                anim.SetFloat("InputX",inputX);
+                anim.SetFloat("InputY",inputY);
+            }
+        }
     }
 }
 
